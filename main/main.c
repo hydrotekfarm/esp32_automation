3d99999263679e43f339858fd4f0dd479cac095c
LAPTOP-9MBV8AE8\rnagp <rnagpal02@outlook.com>
2020-08-25 15:54:51 -0500

    Cleaning up main.c

--- main/main.c
+++ main/main.c
@@ -1,22 +1,6 @@
-#include <inttypes.h>
-#include <freertos/FreeRTOS.h>
-#include <freertos/event_groups.h>
-#include <freertos/semphr.h>
-#include <esp_wifi.h>
-#include <esp_system.h>
-#include <esp_event.h>
-#include <esp_event_loop.h>
-#include <driver/gpio.h>
-#include <esp_err.h>
-#include <freertos/task.h>
-#include <esp_log.h>
-#include <lwip/sockets.h>
-#include <lwip/dns.h>
-#include <lwip/netdb.h>
-#include <stdbool.h>
-
 #include "boot.h"
 
-void app_main(void) {							// Main Method
+void app_main(void) {	// Main Method
+	// Initiate boot
 	boot_sequence();
 }
