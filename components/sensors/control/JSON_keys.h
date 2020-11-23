9122b820bc3dccc284d60609ade740c72388f78e
LAPTOP-9MBV8AE8\rnagp <rnagpal02@outlook.com>
2020-11-23 16:11:19 -0600

    Fixed json string keys

--- components/sensors/control/JSON_keys.h
+++ components/sensors/control/JSON_keys.h
@@ -2,30 +2,30 @@
 #define JSON_KEYS_H
 
 // General keys
-static const char *MONITORING_ONLY = "monitoring_only";
-static const char *CONTROL = "control";
-static const char *DOSING_TIME = "dosing_time";
-static const char *DOSING_INTERVAL = "dosing_interval";
-static const char *DAY_AND_NIGHT = "day_and_night";
-static const char *DAY_TARGET_VALUE = "day_target_value";
-static const char *NIGHT_TARGET_VALUE = "night_target_value";
-static const char *TARGET_VALUE = "target_value";
-static const char *PUMPS = "pumps";
-static const char *ALARM_MIN = "alarm_min";
-static const char *ALARM_MAX = "alarm_max";
+#define MONITORING_ONLY "monitoring_only"
+#define CONTROL "control"
+#define DOSING_TIME "dosing_time"
+#define DOSING_INTERVAL "dosing_interval"
+#define DAY_AND_NIGHT "day_and_night"
+#define DAY_TARGET_VALUE "day_target_value"
+#define NIGHT_TARGET_VALUE "night_target_value"
+#define TARGET_VALUE "target_value"
+#define PUMPS "pumps"
+#define ALARM_MIN "alarm_min"
+#define ALARM_MAX "alarm_max"
 
 // pH specific keys
-static const char *PH_UP_DOWN = "ph_up_down";
-static const char *PUMP_1_ENABLED = "pump_1_enabled";
-static const char *PUMP_2_ENABLED = "pump_2_enabled";
+#define PH_UP_DOWN "ph_up_down"
+#define PUMP_1_ENABLED "pump_1_enabled"
+#define PUMP_2_ENABLED "pump_2_enabled"
 
 // ec specific keys
-static const char *PUMP_1 = "pump_1";
-static const char *PUMP_2 = "pump_2";
-static const char *PUMP_3 = "pump_3";
-static const char *PUMP_4 = "pump_4";
-static const char *PUMP_5 = "pump_5";
-static const char *ENABLED = "enabled";
-static const char *VALUE = "value";
+#define PUMP_1 "pump_1"
+#define PUMP_2 "pump_2"
+#define PUMP_3 "pump_3"
+#define PUMP_4 "pump_4"
+#define PUMP_5 "pump_5"
+#define ENABLED "enabled"
+#define VALUE "value"
 
 #endif
