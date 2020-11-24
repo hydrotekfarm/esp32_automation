34906970d33d2bf82dc835288d0cbc473f867172
LAPTOP-9MBV8AE8\rnagp <rnagpal02@outlook.com>
2020-11-24 11:50:42 -0600

    Fixed acccess point bug

--- components/boot/app_connect.c
+++ components/boot/app_connect.c
@@ -98,9 +98,10 @@ void start_access_point_mode() {
 }
 
 void init_app_connect() {
-	bool has_properties = true; // TODO check if ssid, pw, and broker ip are stored in NVS
+	bool has_properties = false; // TODO check if ssid, pw, and broker ip are stored in NVS
 	if(has_properties) return;
 
+	ESP_ERROR_CHECK(esp_event_loop_create_default());
 	json_information_event_group = xEventGroupCreate();
 	const wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
 
