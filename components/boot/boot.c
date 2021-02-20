4f0a98a19a18163d647c575586fc454fe4314e85
Rajin <rnagpal02@outlook.com>
2021-02-20 16:17:44 -0600

    Suspending tasks on off grow cycle

--- components/boot/boot.c
+++ components/boot/boot.c
@@ -40,9 +40,6 @@ void boot_sequence() {
 	tcpip_adapter_init();
 	ESP_ERROR_CHECK(esp_event_loop_create_default());
 
-	// Init grow manager
-	init_grow_manager();
-
 	// Init network properties
 	init_network_connections();
 
@@ -61,8 +58,11 @@ void boot_sequence() {
 	init_rtc();
 	check_rtc_reset();
 
+	vTaskPrioritySet(NULL, configMAX_PRIORITIES-1);
+
 	// Create core 0 tasks
 	xTaskCreatePinnedToCore(rf_transmitter, "rf_transmitter_task", 2500, NULL, RF_TRANSMITTER_TASK_PRIORITY, &rf_transmitter_task_handle, 0);
+	//vTaskSuspend(rf_transmitter_task_handle);
 	xTaskCreatePinnedToCore(manage_timers_alarms, "timer_alarm_task", 2500, NULL, TIMER_ALARM_TASK_PRIORITY, &timer_alarm_task_handle, 0);
 	xTaskCreatePinnedToCore(publish_data, "publish_task", 2500, NULL, MQTT_PUBLISH_TASK_PRIORITY, &publish_task_handle, 0);
 	xTaskCreatePinnedToCore(sensor_control, "sensor_control_task", 3000, NULL, SENSOR_CONTROL_TASK_PRIORITY, &sensor_control_task_handle, 0);
@@ -72,6 +72,9 @@ void boot_sequence() {
 	xTaskCreatePinnedToCore(measure_ec, "ec_task", 2500, NULL, EC_TASK_PRIORITY, sensor_get_task_handle(get_ec_sensor()), 1);
 	xTaskCreatePinnedToCore(measure_ph, "ph_task", 2500, NULL, PH_TASK_PRIORITY, sensor_get_task_handle(get_ph_sensor()), 1);
 	xTaskCreatePinnedToCore(sync_task, "sync_task", 2500, NULL, SYNC_TASK_PRIORITY, &sync_task_handle, 1);
+
+	// Init grow manager
+	init_grow_manager();
 }
 
 void restart_esp32() { // Restart ESP32
--- components/grow_manager/grow_manager.c
+++ components/grow_manager/grow_manager.c
@@ -2,9 +2,19 @@
 
 #include <esp_log.h>
 #include <esp_err.h>
+#include <FreeRTOS/freertos.h>
+#include <freertos/task.h>
 
 #include "nvs_manager.h"
 #include "nvs_namespace_keys.h"
+#include "ec_reading.h"
+#include "ph_reading.h"
+#include "water_temp_reading.h"
+#include "sync_sensors.h"
+#include "mqtt_manager.h"
+#include "control_task.h"
+#include "rf_transmitter.h"
+#include "rtc.h"
 
 void init_grow_manager() {
 	uint8_t status; // Holds vars coming from NVS
@@ -38,23 +48,54 @@ void push_grow_settings_status() {
 	nvs_add_data(data, GROW_ACTIVE_KEY, UINT8, &is_settings_received);
 }
 
+void suspend_tasks() {
+	// Core 0 tasks
+	vTaskSuspend(rf_transmitter_task_handle);
+	vTaskSuspend(timer_alarm_task_handle);
+	vTaskSuspend(publish_task_handle);
+	vTaskSuspend(sensor_control_task_handle);
+
+	// Core 1
+	vTaskSuspend(*sensor_get_task_handle(get_water_temp_sensor()));
+	vTaskSuspend(*sensor_get_task_handle(get_ec_sensor()));
+	vTaskSuspend(*sensor_get_task_handle(get_ph_sensor()));
+	vTaskSuspend(sync_task_handle);
+}
+
+void resume_tasks() {
+	// Core 0 tasks
+	vTaskResume(rf_transmitter_task_handle);
+	vTaskResume(timer_alarm_task_handle);
+	vTaskResume(publish_task_handle);
+	vTaskResume(sensor_control_task_handle);
+
+	// Core 1
+	vTaskResume(*sensor_get_task_handle(get_water_temp_sensor()));
+	vTaskResume(*sensor_get_task_handle(get_ec_sensor()));
+	vTaskResume(*sensor_get_task_handle(get_ph_sensor()));
+	vTaskResume(sync_task_handle);
+}
+
+
 void start_grow_cycle() {
 	// Don't start unless settings have been received
 	if(!is_settings_received) return;
 
 	// Set active to true and store in NVS
-	ESP_LOGI("", "Starting Grow Cycle");
-
 	is_grow_active = true;
 	push_grow_status();
+
+	resume_tasks();
+	ESP_LOGI("", "Started Grow Cycle");
 }
 
 void stop_grow_cycle() {
 	// Set active to false ands store in NVS
-	ESP_LOGI("", "Stopping Grow Cycle");
-
 	is_grow_active = false;
 	push_grow_status();
+
+	ESP_LOGI("", "Stopped Grow Cycle");
+	suspend_tasks();
 }
 
 void settings_received() {
