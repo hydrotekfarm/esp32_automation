0dab394e6c2c92bf458f09d2ee53110069ebe195
Karthick Sivasubramanian <karthickxs01@gmail.com>
2021-06-07 13:22:59 -0500

    Created climate_controller code base and modifications

--- components/boot/boot.c
+++ components/boot/boot.c
@@ -13,14 +13,9 @@
 
 #include "task_priorities.h"
 #include "ports.h"
-#include "ec_reading.h"
-#include "ph_reading.h"
-#include "water_temp_reading.h"
 #include "sync_sensors.h"
 #include "reservoir_control.h"
 #include "control_task.h"
-#include "ec_control.h"
-#include "ph_control.h"
 #include "control_task.h"
 #include "rtc.h"
 #include "rf_transmitter.h"
@@ -33,52 +28,49 @@
 
 void boot_sequence() {
 	// Init nvs
-	init_nvs();
+	//init_nvs();
 
 	// Initialize deep sleep
 	init_power_button();
 
 	// Init connections
-	tcpip_adapter_init();
-	ESP_ERROR_CHECK(esp_event_loop_create_default());
+	//tcpip_adapter_init();
+	//ESP_ERROR_CHECK(esp_event_loop_create_default());
 
 	// Init network properties
-	init_network_connections();
+	//init_network_connections();
 
-	sensor_event_group = xEventGroupCreate();
+	//sensor_event_group = xEventGroupCreate();
 
 	// Init i2cdev
 	ESP_ERROR_CHECK(i2cdev_init());
 
-	init_ports();
-	is_day = true;
+	//init_ports();
+	//is_day = true;
 
 	// Set all sync bits var
-	set_sensor_sync_bits();
+	//set_sensor_sync_bits();
 
 	// Init time rtc
-	init_sntp();
-	init_rtc();
+	//init_sntp();
+	//init_rtc();
 
 	// Init sensor control
-	init_control();
+	//init_control();
 
 	//Init irrigation
-	init_irrigation();
+	//init_irrigation();
 
 	vTaskPrioritySet(NULL, configMAX_PRIORITIES-1);
 
 	// Create core 0 tasks
-	xTaskCreatePinnedToCore(rf_transmitter, "rf_transmitter_task", 2500, NULL, RF_TRANSMITTER_TASK_PRIORITY, &rf_transmitter_task_handle, 0);
-	xTaskCreatePinnedToCore(manage_timers_alarms, "timer_alarm_task", 2500, NULL, TIMER_ALARM_TASK_PRIORITY, &timer_alarm_task_handle, 0);
-	xTaskCreatePinnedToCore(publish_sensor_data, "publish_task", 2500, NULL, MQTT_PUBLISH_TASK_PRIORITY, &publish_task_handle, 0);
-	xTaskCreatePinnedToCore(sensor_control, "sensor_control_task", 3000, NULL, SENSOR_CONTROL_TASK_PRIORITY, &sensor_control_task_handle, 0);
+	//xTaskCreatePinnedToCore(rf_transmitter, "rf_transmitter_task", 2500, NULL, RF_TRANSMITTER_TASK_PRIORITY, &rf_transmitter_task_handle, 0);
+	//xTaskCreatePinnedToCore(manage_timers_alarms, "timer_alarm_task", 2500, NULL, TIMER_ALARM_TASK_PRIORITY, &timer_alarm_task_handle, 0);
+	//xTaskCreatePinnedToCore(publish_sensor_data, "publish_task", 2500, NULL, MQTT_PUBLISH_TASK_PRIORITY, &publish_task_handle, 0);
+	//xTaskCreatePinnedToCore(sensor_control, "sensor_control_task", 3000, NULL, SENSOR_CONTROL_TASK_PRIORITY, &sensor_control_task_handle, 0);
 
 	// Create core 1 tasks
-	xTaskCreatePinnedToCore(measure_water_temperature, "temperature_task", 2500, NULL, WATER_TEMPERATURE_TASK_PRIORITY, sensor_get_task_handle(get_water_temp_sensor()), 1);
-	xTaskCreatePinnedToCore(measure_ec, "ec_task", 2500, NULL, EC_TASK_PRIORITY, sensor_get_task_handle(get_ec_sensor()), 1);
-	xTaskCreatePinnedToCore(measure_ph, "ph_task", 2500, NULL, PH_TASK_PRIORITY, sensor_get_task_handle(get_ph_sensor()), 1);
-	xTaskCreatePinnedToCore(sync_task, "sync_task", 2500, NULL, SYNC_TASK_PRIORITY, &sync_task_handle, 1);
+	xTaskCreatePinnedToCore(co2_task, "co2_task", 2500, NULL, CO2_TASK_PRIORITY, sensor_get_task_handle(get_co2_sensor()), 1);
 
 
 	// Init grow manager
