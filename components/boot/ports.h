01f192e55b2f81eef9e739b28e200e95ccd52716
Karthick Sivasubramanian <karthickxs01@gmail.com>
2021-07-02 19:30:06 -0500

    returned old rf lib code

--- components/boot/ports.h
+++ components/boot/ports.h
@@ -4,6 +4,5 @@
 #define POWER_BUTTON_GPIO			2
 #define SDA_GPIO 					21
 #define SCL_GPIO 					22
-#define RF_TX_PIN                   17
-#define RF_RX_PIN                   16
+#define RF_TRANSMITTER_GPIO 		4
 
