7cc329e386cb6a9b8e964fbd2ac960e85fb0d92f
Karthick Sivasubramanian <karthickxs01@gmail.com>
2021-07-11 11:07:52 -0500

    debug

--- components/boot/hard_reset_manager.c
+++ components/boot/hard_reset_manager.c
@@ -26,21 +26,20 @@ unsigned long get_current_time() {
 }
 
 static void IRAM_ATTR reset_button_isr_handler (void* arg) {
-    ESP_LOGI(HARD_RESET_TAG, "Entered Interupt Handler.");
+    //ESP_LOGI(HARD_RESET_TAG, "Entered Interupt Handler.");
    unsigned long start = get_current_time();
    //Make sure we get valid start time // 
-   while (start == 0) {
-       start = get_current_time();
-   }
    //Keep checking if button is pressed at lest 10 seconds then perform reset tasks//
-   while (gpio_get_level(HARD_RESET_GPIO) == 1) {
+    while (gpio_get_level(HARD_RESET_GPIO) == 0) {
         unsigned long curr_time = get_current_time();
-        ESP_LOGI(HARD_RESET_TAG, "Start Time: %li... Curr Time: %li", start, curr_time);
-        if (curr_time != 0 && (curr_time - start >= 10)) {
-            ESP_LOGI(HARD_RESET_TAG, "Hard Rest Initiated.");
+
+        //ESP_LOGI(HARD_RESET_TAG, "Start Time: %li... Curr Time: %li", start, curr_time);
+        if (curr_time != 0 && (curr_time - start >= 2)) {
+            //ESP_LOGI(HARD_RESET_TAG, "Hard Rest Initiated.");
+            ets_printf("YEAH");
             nvs_clear();
-            esp_restart();
-            ESP_LOGI(HARD_RESET_TAG, "Hard Rest Completed.");
+            //esp_restart();
+            //ESP_LOGI(HARD_RESET_TAG, "Hard Rest Completed.");
             break; 
         }
    }
@@ -50,9 +49,10 @@ void init_hard_reset_button() {
 	// Create Rising Edge Interrupt on Hard Reset Button GPIO
 	gpio_config_t gpio_conf;
 	gpio_conf.intr_type = GPIO_PIN_INTR_POSEDGE;
-	gpio_conf.pin_bit_mask = GPIO_INPUT_PIN_SEL;
+	gpio_conf.pin_bit_mask = GPIO_INPUT_PIN_SEL_RESET;
 	gpio_conf.mode = GPIO_MODE_INPUT;
 	gpio_conf.pull_up_en = 1;
+    //gpio_conf.pull_down_en = 1;
 	gpio_config(&gpio_conf);
 
 	// Install GPIO ISR Service
@@ -60,4 +60,6 @@ void init_hard_reset_button() {
 
 	// ISR handler for specific GPIO pin
 	gpio_isr_handler_add(HARD_RESET_GPIO, reset_button_isr_handler, (void*) HARD_RESET_GPIO);
+
+    printf("initialized");
 }
