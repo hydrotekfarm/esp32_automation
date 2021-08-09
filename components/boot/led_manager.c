715535b158541f32003e63a554e5e71ac2e94075
Karthick Sivasubramanian <karthickxs01@gmail.com>
2021-08-09 15:03:22 -0500

    updates

--- components/boot/led_manager.c
+++ components/boot/led_manager.c
@@ -9,6 +9,8 @@ void wifi_led(void *args) {
 	gpio_set_direction(GREEN_LED, GPIO_MODE_OUTPUT);
 	gpio_set_level(GREEN_LED, 1);
     gpio_set_direction(BLUE_LED, GPIO_MODE_OUTPUT);
+    printf("Done\n");
+    gpio_set_level(BLUE_LED, 1);
     for (;;) {
         if (get_is_wifi_connected()) {
             gpio_set_level(BLUE_LED, 1);
